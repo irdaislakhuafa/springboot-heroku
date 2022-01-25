8077965569eba156322d1ae7a15a21fa96023862
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-26 03:39:49 +0700

    add page 2

--- src/main/java/com/irdaislakhuafa/springbootheroku/controllers/IndexController.java
+++ src/main/java/com/irdaislakhuafa/springbootheroku/controllers/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/springboot_heroku")
@@ -17,5 +18,11 @@ public class IndexController {
             e.printStackTrace();
         }
         return "index";
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String page2() {
+        return "<h1>Page 2</h1>";
     }
 }

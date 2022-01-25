7eb641282c6b7d5c6547ee4121ae5427d83ef911
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-26 02:54:19 +0700

    try edit controller

--- src/main/java/com/irdaislakhuafa/springbootheroku/controllers/IndexController.java
+++ src/main/java/com/irdaislakhuafa/springbootheroku/controllers/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/springboot_heroku")
+@RequestMapping("/")
 public class IndexController {
     @GetMapping
     public String index(Model model) {
